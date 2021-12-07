f0a815bb9d5e9144928845c06f175b95ab1c0088
andreasvikke <azv@efio.dk>
2021-12-07 17:12:34 +0100

    Fixed Neo4J

--- terraform/modules/neo4j/main.tf
+++ terraform/modules/neo4j/main.tf
@@ -8,30 +8,36 @@ resource "helm_release" "neo4j" {
   name       = "neo4j"
   namespace  = kubernetes_namespace.neo4j.metadata.0.name
 
-  repository = "https://helm.neo4j.com/neo4j"
-  chart      = "neo4j-standalone"
+  chart      = "${path.module}/chart/"
   timeout    = 900
 
-  values = [
-    "${file("${path.module}/custom-values.yaml")}"
-  ]
-
+#   values = [
+#     "${file("${path.module}/custom-values.yaml")}"
+#   ]
   set {
     name  = "neo4jPassword"
     value = "P@ssword!"
   }
-}
-
-data "kubernetes_service" "neo4j" {
-  metadata {
-    name      = "neo4j-neo4j"
-    namespace = kubernetes_namespace.neo4j.metadata.0.name
+  set {
+    name  = "acceptLicenseAgreement"
+    value = "yes"
   }
-
-  depends_on = [
-    helm_release.neo4j
-  ]
+  set {
+    name  = "core.service.type"
+    value = "LoadBalancer"
+  }
 }
 
+# data "kubernetes_service" "neo4j" {
+#   metadata {
+#     name      = "neo4j-neo4j"
+#     namespace = kubernetes_namespace.neo4j.metadata.0.name
+#   }
 
-# kubectl run --rm -it --image "neo4j:4.3.2-enterprise" cypher-shell -- cypher-shell -a "neo4j://neo4j.neo4j.svc.cluster.local:7687" -u neo4j -p "P@ssword\!"
+#   depends_on = [
+#     helm_release.neo4j
+#   ]
+# }
+
+
+# kubectl run --rm -it --image "neo4j:4.3.2-enterprise" cypher-shell -- cypher-shell -a "neo4j://neo4j.neo4j-si.svc.cluster.local:7687" -u neo4j -p "P@ssword\!"
--- terraform/modules/neo4j/output.tf
+++ terraform/modules/neo4j/output.tf
@@ -1,3 +1,3 @@
 output "neo4j_web_ip" {
-    value = "${data.kubernetes_service.neo4j.status.0.load_balancer.0.ingress.0.ip}:7474"
+    value =  "t" #"${data.kubernetes_service.neo4j.status.0.load_balancer.0.ingress.0.ip}:7474"
 }
