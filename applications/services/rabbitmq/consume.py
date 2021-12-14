335bc7f8c5ef414a53c1c076f242e36461f6773c
Asger Hermind Sørensen <43413829+asgerhs@users.noreply.github.com>
2021-12-14 14:31:16 +0100

    main.tf fix, added proto.py

--- applications/services/rabbitmq/consume.py
+++ applications/services/rabbitmq/consume.py
@@ -1,28 +1,20 @@
 import pika
-import psycopg2
+import loan_pb2
 
 def main():
     rabbitmqConnection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = rabbitmqConnection.channel()
 
-    postgresconnection = psycopg2.connect(
-    host="localhost",
-    database="suppliers",
-    user="postgres",
-    password="P@ssword!")
-
-    cursor = postgresconnection.cursor()
 
     channel.queue_declare(queue='BorrowerQueue')
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
-        sql = """INSERT INTO loan(loaner_id, entity_id)
-                VALUES(%s, %s)"""
-        cursor.execute(sql, (body[0], body[1]))
+        loan_pb2.Loan.userId = body[0]
+        loan_pb2.Loan.entityId = body[1]
+        loan_pb2.Loan.status = 0
 
-
-    channel.basic_consume(queue='hello',
+    channel.basic_consume(queue='BorrowerQueue',
                         auto_ack=True,
                         on_message_callback=callback)
 
